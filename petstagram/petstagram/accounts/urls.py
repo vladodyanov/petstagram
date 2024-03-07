c8b3655fc66ed9c5df745432989d11de6ab2332c
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-03-07 14:23:32 +0200

    Creating views for user signup, signin

--- petstagram/petstagram/accounts/urls.py
+++ petstagram/petstagram/accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 
 from petstagram.accounts.views import \
-    signup_user, signin_user, \
+    signin_user, \
     details_profile, delete_profile, \
-    edit_profile, signout_user
+    edit_profile, signout_user, SignupUserView
 
 urlpatterns = (
-    path("signup/", signup_user, name="signup user"),
+    path("signup/", SignupUserView.as_view(), name="signup user"),
     path("signin/", signin_user, name="signin user"),
     path("signout/", signout_user, name="signout user"),
 
--- petstagram/petstagram/accounts/views.py
+++ petstagram/petstagram/accounts/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 
 # Callables:
 # - all functions
 # - objects with overriden `__call__` method
 
-def signup_user(request):
-    context = {}
-    return render(request, "accounts/signup_user.html", context)
+# def signup_user(request):
+#     context = {}
+#     return render(request, "accounts/signup_user.html", context)
+
+class SignupUserView(auth_views.LoginView):
+    template_name = "accounts/signup_user.html"
+
+    def get_success_url(self):
+        return reverse_lazy('index')
 
 
 def signin_user(request):
     context = {}
 
     return render(request, "accounts/signin_user.html", context)
+
+
+
 
 
 def signout_user(request):
